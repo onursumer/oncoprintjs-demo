ba5cc4dcc8d325341e039108dcaab60aa8308a1d
Onur Sumer <s.onur.sumer@gmail.com>
2022-08-04 16:43:45 -0400

    add an oncoprintjs example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
+
 import './App.css';
+import {heatmap} from "./oncoprint-heatmap";
+import {glyphmap} from "./oncoprint-glyphmap";
 
 function App() {
+
+  useEffect(() => {
+      glyphmap();
+      heatmap();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Oncoprint Genomic Alterations</h2>
+      <div id="oncoprint-glyphmap"></div>
+      <h2>Oncoprint Heatmap</h2>
+      <div id="oncoprint-heatmap"></div>
     </div>
   );
 }
